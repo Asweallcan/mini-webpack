7ac01b437914a7b651310bccb819be4b82e39762
shihaolv <shihaolv@autox.ai>
2022-12-28 16:37:30 +0800

    mini webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import person from "./person.js";
+import message from "./message.js";
+
+console.log(`${person.name} said ${message}`);
---
+++ src/message.js
@@ -0,0 +1,5 @@
+const randomNumber = require("./randomNumber.js").default;
+
+exports.test = "this is test";
+
+module.exports = `something random: ${randomNumber()}`;
